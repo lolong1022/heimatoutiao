6e2be01b9e5c1a3e08d13584e4546a6c2b5ca2e3
jsli <2636530377@qq.com>
2022-09-06 18:14:05 +0800

    login半

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <template>
   <div>
-<van-button type="primary">主要按钮</van-button>
-<van-button type="info">信息按钮</van-button>
-<van-button type="default">默认按钮</van-button>
-<van-button type="warning">警告按钮</van-button>
-<van-button type="danger">危险按钮</van-button>
-<!-- 调试rem盒子 -->
-<!-- <div class="box">123</div> -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 // 3.配置rem动起来
-export default {
-}
-
+export default {}
 </script>
 
-<style lang='less' scoped>
-  // 调试rem
+<style lang="less" scoped>
+// 调试rem
 // .box {
 //     width: 2rem;
 //     height: 2rem;
---
+++ src/views/Login/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <!-- 头部导航 -->
+    <van-nav-bar class="login-bar" title="登录" />
+    <!-- form表单 -->
+    <van-form @submit="onSubmit" class="from"
+      >
+      <van-field
+        v-model="mobile"
+        name="mobile"
+        placeholder="请输入手机号"
+        :rules="[
+          { required: true, message: '请填写手机号' },
+          {
+            pattern:
+              /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-79])|(?:5[0-35-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[189]))\d{8}$/,
+            message: '手机号格式不正确'
+          }
+        ]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-shouji"></span>
+        </template>
+      </van-field>
+      <van-field
+        v-model="code"
+        name="code"
+        placeholder="请输入验证码"
+        :rules="[
+          { required: true, message: '请填写验证码' },
+          {
+            pattern: /[0-9]{6}/,
+            message: '验证码格式不正确'
+          }
+        ]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-yanzhengma"></span> </template
+      ></van-field>
+      <div style="margin: 16px">
+        <van-button block type="info" native-type="submit">登录</van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mobile: '',
+      code: ''
+    }
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-bar {
+  background-color: #3296fa;
+  :deep(.van-nav-bar__title) {
+    color: #fff;
+  }
+}
+:deep(.from) {
+    .van-cell__title{
+        flex:1
+    }
+    .van-cell__value {
+        flex:19
+    }
+    .toutiao {
+        font-size: 40px;
+    }
+  }
+</style>
